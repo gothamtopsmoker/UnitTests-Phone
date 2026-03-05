078ca8dfcf2c88909b68307505c21b9b0e0a29a4
Maksym Heraskin <maksym.heraskin@microsoft.wsei.edu.pl>
2026-03-05 21:50:24 +0100

    fixed Assert.ThrowsException error, 5 tests 44 covered blocks

--- PhoneSolution/TestProjectPhone/Test1.cs
+++ PhoneSolution/TestProjectPhone/Test1.cs
@@ -1,11 +1,41 @@
-﻿namespace TestProjectPhone
+﻿using System;
+using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestProjectPhone
 {
     [TestClass]
-    public sealed class Test1
+    public class PhoneTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Konstructor_PoprawneDane_TworzyObiekt()
         {
+            var phone = new Phone("John Doe", "123456789");
+            Assert.AreEqual("John Doe", phone.Owner);
+            Assert.AreEqual("123456789", phone.PhoneNumber);
+            Assert.AreEqual(0, phone.Count);
         }
+
+        [TestMethod]
+        public void Konstructor_PustyOwner_RzucaWyjatek()
+        {
+            Assert.ThrowsException<ArgumentException>(() => new Phone("", "123456789"));
+        }
+        [TestMethod]
+        public void Konstructor_NullOwner_RzucaWyjatek()
+        {
+            Assert.ThrowsException<ArgumentException>(() => new Phone(null, "123456789"));
+        }
+        [TestMethod]
+        public void Konstructor_PustyPhoneNumber_RzucaWyjatek()
+        {
+            Assert.ThrowsException<ArgumentException>(() => new Phone("John Doe", ""));
+        }
+        [TestMethod]
+        public void Konstructor_NullPhoneNumber_RzucaWyjatek()
+        {
+            Assert.ThrowsException<ArgumentException>(() => new Phone("John Doe", null));
+        }
+
     }
 }
